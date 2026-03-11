391c3b15f149846275a88c8a8a61d4d5a278c152
Laura Camacho <lauracamacholeon@gmail.com>
2026-03-11 18:30:57 -0500

    feat: add global styles and SCSS variables

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'funds',
+    pathMatch: 'full',
+  },
+  {
+    /** Funds feature module - lazy loaded */
+    path: 'funds',
+    loadComponent: () =>
+      import('./features/funds/pages/funds-page/funds-page.component').then(
+        (m) => m.FundsPageComponent
+      ),
+  },
+  {
+    /** History feature module - lazy loaded */
+    path: 'history',
+    loadComponent: () =>
+      import(
+        './features/history/pages/history-page/history-page.component'
+      ).then((m) => m.HistoryPageComponent),
+  },
+  {
+    /** Redirect any unknown route to funds */
+    path: '**',
+    redirectTo: 'funds',
+  },
+];
